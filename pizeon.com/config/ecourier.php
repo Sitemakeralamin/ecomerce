de3bb28c06d26698b2cc22a1e590e54348bc3f92
Sohag <80314649+shshohagh@users.noreply.github.com>
2025-01-11 11:30:54 +0600

    remove

--- pizeon.com/config/ecourier.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    "sandbox"    => env("ECOURIER_SANDBOX", false),
-    "app_key"    => env("ECOURIER_API_KEY", ""),
-    "app_secret" => env("ECOURIER_API_SECRET", ""),
-    "user_id"    => env("ECOURIER_USER_ID", "")
-];
