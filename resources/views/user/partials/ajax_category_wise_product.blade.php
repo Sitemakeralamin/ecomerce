25823c5edc90f5800e65216e81c4e36ec73e36d6
Sohag <80314649+shshohagh@users.noreply.github.com>
2025-01-27 11:31:21 +0600

    Update ajax_category_wise_product.blade.php

--- resources/views/user/partials/ajax_category_wise_product.blade.php
+++ resources/views/user/partials/ajax_category_wise_product.blade.php
@@ -54,6 +54,17 @@
                               </a>
                         </div>
                       </div>
+                      <div class="section__heading mb-2 border-bottom d-flex">
+                        <h2 class="section__heading-- style2 flex-grow-1">{{ $category->title }}</h2>
+                        <div class="btn_custom mb-2">
+                            <a class=" rounded shop_more_btn" 
+                            href="{{route('products', ['category_id'=>$category->id])}}">Shop More
+                                <svg class="primary__btn--arrow__icon" xmlns="http://www.w3.org/2000/svg" width="20.2" height="12.2" viewBox="0 0 6.2 6.2">
+                                <path d="M7.1,4l-.546.546L8.716,6.713H4v.775H8.716L6.554,9.654,7.1,10.2,9.233,8.067,10.2,7.1Z" transform="translate(-4 -4)" fill="currentColor"></path>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
 
                       <div class="product__section--inner">
                         <div class="row row-cols-xl-5 row-cols-lg-5 row-cols-md-3 row-cols-2 mb--n30">
